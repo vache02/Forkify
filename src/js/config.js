8d92facbcb09ad269fa145e89e271b32faf98d07
Davit <vacheishvilidavit2@gmail.com>
2024-03-04 08:24:41 +0400

    initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b04bf004-c58a-4d72-bbd3-1340a7e20c14';
+export const MODAL_CLOSE_SEC = 2.5;
